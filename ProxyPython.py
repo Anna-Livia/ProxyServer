b3f0a359e057d8e7fdc1971de7669e1e4eb82dff
Anna-Livia <annalivia.cardin@gmail.com>
2017-04-14 18:19:48 +0200

    logs url accessed and seperate info flux

--- ProxyPython.py
+++ ProxyPython.py
@@ -11,6 +11,31 @@ config =  {
             "CONNECTION_TIMEOUT" : 5
           }
 
+def HTTP_request_he_to_she(http_request):
+    lines = http_request.split('\n')
+    index = False
+    text = ""
+    for i in range(len(lines)) :
+        if lines[i] == "" :
+            try :
+                index = i+1
+                text = lines[i+1]
+            except IndexError :
+                return False
+
+        if "Content-Type: image" in lines[i] :
+            return False
+
+    text.replace(" he ", " she ")
+    print(text)
+    if index != False :
+        lines[index] = text
+    else :
+        return False
+    return lines
+
+
+
 
 class Server:
     """ The server class """
@@ -43,10 +68,18 @@ class Server:
         *******************************************
         """
 
-        request = conn.recv(config['MAX_REQUEST_LEN'])        # get the request from browser
-        first_line = request.split('\n')[0]                   # parse the first line
-        url = first_line.split(' ')[1]                        # get url
-        print(url)
+        request_from_browser = conn.recv(config['MAX_REQUEST_LEN'])        # get the request from browser
+        request_from_proxy = request_from_browser
+
+        first_line = request_from_browser.split('\n')[0]  
+        print("######################")
+        print(first_line[:50])
+        try :                 # parse the first line
+            url = first_line.split(' ')[1]   
+        except IndexError :
+            url = ""                     # get url
+        print(url[:50])
+        print("######################")
         # find the webserver and port
         http_pos = url.find("://")          # find pos of ://
         if (http_pos==-1):
@@ -75,12 +108,14 @@ class Server:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.settimeout(config['CONNECTION_TIMEOUT'])
             s.connect((webserver, port))
-            s.sendall(request)                           # send request to webserver
+            s.sendall(request_from_proxy)                           # send request to webserver
 
             while 1:
-                data = s.recv(config['MAX_REQUEST_LEN'])          # receive data from web server
-                if (len(data) > 0):
-                    conn.send(data)                               # send to browser
+                data_from_server = s.recv(config['MAX_REQUEST_LEN'])          # receive data from web server
+                data_from_proxy = data_from_server
+                #HTTP_request_he_to_she(data_from_proxy)
+                if (len(data_from_proxy) > 0):
+                    conn.send(data_from_proxy)                               # send to browser
                 else:
                     break
             s.close()
